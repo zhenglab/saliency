4a4fdda2cce7f91e9cb59617a55368b9fe7b8a8b
zhuyafei <zhuyafei4520@163.com>
2015-08-17 16:28:14 +0800

    Modify locationToMap.m

--- code_forMetrics/locationToMap.m
+++ code_forMetrics/locationToMap.m
@@ -7,7 +7,7 @@ imgNum = length(fixCell);
 for i = 1:imgNum
     hight = sizeData(i, 1);
     width = sizeData(i, 2);
-    fixationMap = zeros(hight, width);
+    fixLocs = zeros(hight, width);
     fixations = fixCell{i, 1};
     fixationLocation = round(fixations(:, 1:2));
     validPos = fixationLocation(:,1)>0 & fixationLocation(:,2)>0 & fixationLocation(:,1)<=hight & fixationLocation(:,2)<=width;
@@ -16,9 +16,9 @@ for i = 1:imgNum
     for j = 1:m
        x = fixationLocation(j, 1);
        y = fixationLocation(j, 2);
-       fixationMap(x, y) = 1;
+       fixLocs(x, y) = 1;
     end
-    %imgsal{1, i} = fixationMap;
-    eval(['save ', strcat('./imgsal/', num2str(i), '.mat'), ' fixationMap'])
+    %imgsal{1, i} = fixLocs;
+    eval(['save ', strcat('./imgsal/', num2str(i), '.mat'), ' fixLocs'])
 end
 %save judd judd -v7.3
